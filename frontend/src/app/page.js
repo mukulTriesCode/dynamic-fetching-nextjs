0230f44612101dd57c646d95b6a0e7acc3ce57f7
Mukul Sharma <mukulsharma262002@gmail.com>
2024-12-02 18:07:31 +0530

    Frontend: Added heading in page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,11 @@
+import Heading from "@/components/Heading";
+
 export default function Home() {
-  return <main>
-    <h1 className="text-6xl font-bold text-center">Welcome to Next.js!</h1>
-  </main>;
+  return (
+    <main>
+      <h1 className="text-6xl font-bold text-center">
+        <Heading />
+      </h1>
+    </main>
+  );
 }
