c10175b2e430e8b16fa83c1a83ebc684798d02dd
Mukul Sharma <mukulsharma262002@gmail.com>
2024-12-02 16:44:08 +0530

    Frontend: Created Navbar component and added in layout

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-slate-200">
+      <div className="container flex justify-between py-5 px-2 text-xl">
+        <div className="">Logo</div>
+        <div className="">
+          <ul className="flex gap-8">
+            <li className="w-fit hover:underline"><Link href="/about">About</Link></li>
+            <li className="w-fit hover:underline"><Link href="/blogs">Blogs</Link></li>
+            <li className="w-fit hover:underline"><Link href="/contact">Contact Us</Link></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
